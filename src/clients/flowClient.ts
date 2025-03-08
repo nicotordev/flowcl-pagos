843b4e6d2b945b5f77bc73c2117b65aff4f783da
Nicolas <nicotordev@gmail.com>
2025-03-08 12:38:26 -0300

    Agregar métodos para obtener el estado de pagos y listar pagos recibidos por fecha en FlowClient

--- src/clients/flowClient.ts
+++ src/clients/flowClient.ts
@@ -7,22 +7,67 @@ import axios, { AxiosInstance } from 'axios';
 import CryptoJS from 'crypto-js';
 
 import {
-  FlowCreatePaymentRequest,
-  FlowCreatePaymentResponse,
+  FlowPaymentsReceivedByDateRequest,
+  FlowPaymentsReceivedByDateResponse,
   FlowPaymentStatusResponse,
 } from '../types/flow';
-import { getPaymentMethod, getPaymentStatus } from '../utils/flow.utils';
+import {
+  getPaymentStatus,
+  isValidPaymentReceivedByDate,
+} from '../utils/flow.utils';
 import {
   FlowAPIError,
   FlowAuthenticationError,
-  FlowOrderCreationError,
   FlowPaymentStatusError,
+  PaymentsReceivedByDateError,
 } from '../errors';
 
 class FlowClient {
   private apiKey: string;
   private secretKey: string;
   private axiosInstance: AxiosInstance;
+
+  /**
+   * Objeto que proporciona métodos para interactuar con los pagos en Flow.
+   */
+  public payments: {
+    /**
+     * Métodos para consultar el estado de pagos en Flow mediante diferentes identificadores.
+     */
+    status: {
+      /**
+       * Obtiene el estado de un pago utilizando su token.
+       * @param token Token único del pago a consultar.
+       * @returns Una promesa que resuelve con la respuesta de Flow sobre el estado del pago.
+       * @throws FlowPaymentStatusError Si ocurre un error al obtener el estado del pago.
+       * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
+       */
+      byToken: (token: string) => Promise<FlowPaymentStatusResponse>;
+
+      /**
+       * Obtiene el estado de un pago utilizando el identificador de comercio.
+       * @param commerceId Identificador único del comercio asignado al pago.
+       * @returns Una promesa que resuelve con la respuesta de Flow sobre el estado del pago.
+       * @throws FlowPaymentStatusError Si ocurre un error al obtener el estado del pago.
+       * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
+       */
+      byCommerceId: (commerceId: string) => Promise<FlowPaymentStatusResponse>;
+
+      /**
+       * Obtiene el estado de un pago utilizando el número de orden de Flow.
+       * @param flowOrder Número de orden de Flow asignado al pago.
+       * @returns Una promesa que resuelve con la respuesta de Flow sobre el estado del pago.
+       * @throws FlowPaymentStatusError Si ocurre un error al obtener el estado del pago.
+       * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
+       */
+      byFlowOrderNumber: (
+        flowOrder: number,
+      ) => Promise<FlowPaymentStatusResponse>;
+    };
+    list: (
+      data: FlowPaymentsReceivedByDateRequest,
+    ) => Promise<FlowPaymentsReceivedByDateResponse>;
+  };
 
   /**
    * Constructor de la clase FlowClient.
@@ -54,6 +99,15 @@ class FlowClient {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
     });
+
+    this.payments = {
+      status: {
+        byToken: this.getPaymentStatusByToken.bind(this),
+        byCommerceId: this.getPaymentStatusByCommerceId.bind(this),
+        byFlowOrderNumber: this.getPaymentStatusByFlowOrderNumber.bind(this),
+      },
+      list: this.getPaymentsReceivedByDate.bind(this),
+    };
   }
 
   /**
@@ -61,7 +115,7 @@ class FlowClient {
    * @param params Parámetros a firmar.
    * @returns Firma generada.
    */
-  private generateSignature(params: any): string {
+  private generateSignature(params: Record<string, string>): string {
     const sortedKeys = Object.keys(params).sort(); // Ordenar las claves alfabéticamente
     let toSign = '';
     sortedKeys.forEach((key) => {
@@ -71,54 +125,23 @@ class FlowClient {
   }
 
   /**
-   * Crea una orden de pago en Flow.
-   * @param data Datos de la orden de pago (sin incluir apiKey y firma, ya que se agregan automáticamente).
-   * @returns Respuesta de Flow con los detalles de la orden creada.
-   * @throws FlowOrderCreationError Si ocurre un error en la creación de la orden.
-   * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
-   */
-  async createOrder(
-    data: Omit<FlowCreatePaymentRequest, 'apiKey' | 's'>,
-  ): Promise<FlowCreatePaymentResponse> {
-    try {
-      const paymentMethod = getPaymentMethod(data.paymentMethod ?? 'flow');
-      const allData = { ...data, apiKey: this.apiKey, paymentMethod }; // Agregar apiKey a los datos
-      const signature = this.generateSignature(allData); // Generar firma
-      const formData = new URLSearchParams({
-        ...(allData as any),
-        s: signature, // Agregar firma a los datos enviados
-      });
-
-      // Enviar solicitud POST a la API de Flow para crear el pago
-      const response = await this.axiosInstance.post<FlowCreatePaymentResponse>(
-        '/payment/create',
-        formData,
-      );
-      return {
-        ...response.data,
-        redirectUrl: response.data.url + '?token=' + response.data.token,
-      };
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        throw new FlowAPIError(error.response?.status || 500, error.message);
-      }
-      throw new FlowOrderCreationError((error as Error).message);
-    }
-  }
-
-  /**
    * Obtiene el estado de un pago en Flow.
    * @param token Token del pago a consultar.
    * @returns Respuesta de Flow con el estado del pago.
    * @throws FlowPaymentStatusError Si ocurre un error al obtener el estado del pago.
    * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
    */
-  async getPaymentStatus(token: string): Promise<FlowPaymentStatusResponse> {
+  private async getPaymentStatusByToken(
+    token: string,
+  ): Promise<FlowPaymentStatusResponse> {
     try {
-      const allData = { token, apiKey: this.apiKey }; // Agregar apiKey a los datos
+      const allData = { token, apiKey: this.apiKey } as unknown as Record<
+        string,
+        string
+      >;
       const signature = this.generateSignature(allData); // Generar firma
       const formData = new URLSearchParams({
-        ...(allData as any),
+        ...allData,
         s: signature, // Agregar firma a los datos enviados
       });
 
@@ -135,6 +158,115 @@ class FlowClient {
         throw new FlowAPIError(error.response?.status || 500, error.message);
       }
       throw new FlowPaymentStatusError((error as Error).message);
+    }
+  }
+
+  /**
+   * Obtiene el estado de un pago utilizando el identificador de comercio.
+   * @param commerceId Identificador único del comercio asignado al pago.
+   * @returns Una promesa que resuelve con la respuesta de Flow sobre el estado del pago.
+   * @throws FlowPaymentStatusError Si ocurre un error al obtener el estado del pago.
+   * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
+   */
+  private async getPaymentStatusByCommerceId(
+    commerceId: string,
+  ): Promise<FlowPaymentStatusResponse> {
+    try {
+      const allData = { commerceId, apiKey: this.apiKey } as Record<
+        string,
+        string
+      >;
+      const signature = this.generateSignature(allData);
+      const formData = new URLSearchParams({
+        ...allData,
+        s: signature,
+      });
+
+      const response = await this.axiosInstance.get<FlowPaymentStatusResponse>(
+        '/payment/getPaymentStatusByCommerceId?' + formData.toString(),
+      );
+      return {
+        ...response.data,
+        statusStr: getPaymentStatus(response.data.status),
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new FlowAPIError(error.response?.status || 500, error.message);
+      }
+      throw new FlowPaymentStatusError((error as Error).message);
+    }
+  }
+
+  /**
+   * Obtiene el estado de un pago utilizando el número de orden de Flow.
+   * @param flowOrder Número de orden de Flow asignado al pago.
+   * @returns Una promesa que resuelve con la respuesta de Flow sobre el estado del pago.
+   * @throws FlowPaymentStatusError Si ocurre un error al obtener el estado del pago.
+   * @throws FlowAPIError Si la API de Flow responde con un error HTTP.
+   */
+  private async getPaymentStatusByFlowOrderNumber(
+    flowOrder: number,
+  ): Promise<FlowPaymentStatusResponse> {
+    try {
+      const allData = { flowOrder, apiKey: this.apiKey } as unknown as Record<
+        string,
+        string
+      >;
+      const signature = this.generateSignature(allData);
+      const formData = new URLSearchParams({
+        ...allData,
+        s: signature,
+      });
+
+      const response = await this.axiosInstance.get<FlowPaymentStatusResponse>(
+        '/payment/getStatusByFlowOrder?' + formData.toString(),
+      );
+      return {
+        ...response.data,
+        statusStr: getPaymentStatus(response.data.status),
+      };
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new FlowAPIError(error.response?.status || 500, error.message);
+      }
+      throw new FlowPaymentStatusError((error as Error).message);
+    }
+  }
+  private async getPaymentsReceivedByDate(
+    data: FlowPaymentsReceivedByDateRequest,
+  ): Promise<FlowPaymentsReceivedByDateResponse> {
+    try {
+      // check that the data is in right format, if not, throw an error
+      // date should be in format YYYY-MM-DD
+      const validatedDate = isValidPaymentReceivedByDate(data.date);
+
+      if (Boolean(validatedDate) === false) {
+        throw new PaymentsReceivedByDateError('Fecha no válida');
+      }
+
+      const allData = {
+        ...data,
+        apiKey: this.apiKey,
+        date: validatedDate,
+      } as unknown as Record<string, string>;
+
+      const signature = this.generateSignature(allData);
+      const formData = new URLSearchParams({
+        ...allData,
+        s: signature,
+      });
+
+      const response =
+        await this.axiosInstance.get<FlowPaymentsReceivedByDateResponse>(
+          '/payment/getPayments?' + formData.toString(),
+        );
+
+      return response.data;
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        throw new FlowAPIError(error.response?.status || 500, error.message);
+      }
+      throw new PaymentsReceivedByDateError((error as Error).message);
     }
   }
 }
