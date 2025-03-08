2e5207e901beaced968b9e10cf92f2769309889e
Nicolas <nicotordev@gmail.com>
2025-03-08 19:16:42 -0300

    Agregar exportaciones para FlowCustomers, FlowPayments y FlowRefunds en index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-import Flow from './clients/flow';
-
-export { Flow };
+// src/index.ts
+export { default as FlowCustomers } from './clients/flow.customers';
+export { default as FlowPayments } from './clients/flow.payments';
+export { default as FlowRefunds } from './clients/flow.refunds';
+export { default as Flow } from './clients/flow';
