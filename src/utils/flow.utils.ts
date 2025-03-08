b43403bbd23ba027e6b10af5bb2ee3d60a679dca
Nicolas <nicotordev@gmail.com>
2025-03-08 12:36:10 -0300

    Agregar función para validar y formatear fechas de pagos recibidos

--- src/utils/flow.utils.ts
+++ src/utils/flow.utils.ts
@@ -1,5 +1,6 @@
 import flowConstants from '../constants/flow.constants';
 import { FlowPaymentStatus, PaymentMethods } from '../types/flow';
+import { parse, isValid, format } from 'date-fns';
 
 function getPaymentMethod(paymentMethod: PaymentMethods) {
   return flowConstants.FLOW_PAYMENT_METHOD_CODES[paymentMethod];
@@ -9,4 +10,27 @@ function getPaymentStatus(status: number): FlowPaymentStatus {
   return flowConstants.FLOW_PAYMENT_STATUS_CODES[status];
 }
 
-export { getPaymentMethod, getPaymentStatus };
+/**
+ * Verifica si una fecha está en el formato YYYY-MM-DD o intenta convertirla.
+ * @param dateString La fecha en formato string o Date.
+ * @returns La fecha en formato YYYY-MM-DD si es válida, o null si no puede convertirse.
+ */
+function isValidPaymentReceivedByDate(
+  dateString: string | Date,
+): string | null {
+  if (
+    typeof dateString === 'string' &&
+    /^\d{4}-\d{2}-\d{2}$/.test(dateString)
+  ) {
+    return dateString;
+  }
+
+  const parsedDate = parse(dateString.toString(), 'yyyy-MM-dd', new Date());
+  if (isValid(parsedDate)) {
+    return format(parsedDate, 'yyyy-MM-dd');
+  }
+
+  return null;
+}
+
+export { getPaymentMethod, getPaymentStatus, isValidPaymentReceivedByDate };
