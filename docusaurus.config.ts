6ef0f7ca4f51353bc59ce77e2ea10c79812ab283
Nicolas <nicotordev@gmail.com>
2025-03-10 19:32:33 -0300

    Actualizar configuración de Docusaurus para mejorar la consistencia del código y agregar soporte para Algolia

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,58 +1,58 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: '@nicotordev/flowcl-pagos',
-  tagline: 'Flowcl Pagos',
-  favicon: 'https://www.flow.cl/favicon.ico',
+  title: "@nicotordev/flowcl-pagos",
+  tagline: "Flowcl Pagos",
+  favicon: "https://www.flow.cl/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://nicotordev-flowcl-pagos.netlify.app',
-  baseUrl: '/',
+  url: "https://nicotordev-flowcl-pagos.netlify.app",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: '@nicotordev', // Usually your GitHub org/user name.
-  projectName: '@nicotordev/flowcl-pagos', // Usually your repo name.
+  organizationName: "@nicotordev", // Usually your GitHub org/user name.
+  projectName: "@nicotordev/flowcl-pagos", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'es',
-    locales: ['es'],
+    defaultLocale: "es",
+    locales: ["es"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/nicotordev/flowcl-pagos/edit/main/docusaurus/docs/',
+            "https://github.com/nicotordev/flowcl-pagos/edit/main/docusaurus/docs/",
         },
         blog: {
           showReadingTime: true,
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -60,23 +60,23 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'https://www.flow.cl/images/header/logo-flow.svg',
+    image: "https://www.flow.cl/images/header/logo-flow.svg",
     navbar: {
-      title: '@nicotordev/flowcl-pagos',
+      title: "@nicotordev/flowcl-pagos",
       logo: {
-        alt: '@nicotordev/flowcl-pagos Logo',
-        src: 'https://www.flow.cl/favicon.ico',
+        alt: "@nicotordev/flowcl-pagos Logo",
+        src: "https://www.flow.cl/favicon.ico",
       },
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "Docs",
           items: [
             {
-              label: 'Flow.cl',
-              to: 'https://www.flow.cl/docs/api.html#section/Introduccion',
+              label: "Flow.cl",
+              to: "https://www.flow.cl/docs/api.html#section/Introduccion",
             },
           ],
         },
@@ -86,6 +86,11 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.github,
+    },
+    algolia: {
+      appId: "2HZUKBSU5S",
+      apiKey: "d1f6e4ec71d6c805c921ff36f4c1c22c",
+      indexName: "nicotordev-flowcl-pagos",
     },
   } satisfies Preset.ThemeConfig,
 };
